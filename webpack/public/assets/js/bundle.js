200f9042cb90bfb76fe5138e08789a0d629e22cd
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-07-08 20:14:54 -0300

    rt

--- webpack/public/assets/js/bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-var x = 1;
-/******/ })()
-;
-//# sourceMappingURL=bundle.js.map
