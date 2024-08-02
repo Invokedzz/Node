d98c315e2974713580bbb778aab0c137726b53cf
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-02 15:35:01 -0300

    new features

--- meadowlark/lib/handlers.js
+++ meadowlark/lib/handlers.js
@@ -14,7 +14,29 @@ exports.newsletterSignupProcess = (req, res) => {
     res.redirect(303, '/newsletter-signup/thank-you');
 }
 
+exports.api = {
+    newsletterSignup: (req, res) => {
+        console.log('CSRF token: ' + req.body._csrf);
+        console.log('Name: ' + req.body.name);
+        console.log('Email: ' + req.body.email);
+        res.send({result: 'success'});
+    },
+};
+
+exports.vacationPhotoContest = (req, res) => {
+    const now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+  }
+
+exports.vacationPhotoContestProcess = (req, res, fields, files) => {
+        console.log('field data: ', fields);
+        console.log('files: ', files);
+        res.redirect(303, '/contest/vacation-photo-thank-you');
+}
+
 exports.newsletterSignupThankYou = (req, res) => res.render('newsletter-signup-thank-you');
+
+exports.newsletter = (req, res) => res.render('newsletter', {csrf: 'CSRF token goes here'});
 
 exports.sectionTest = (req, res) => res.render('00-home');
 
--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const { engine } = require('express-handlebars');
 const handlers = require('./lib/handlers');
 const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
 app.engine('handlebars', engine({
     defaultLayout: 'main',
     helpers: {
@@ -18,16 +19,31 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', handlers.home);
 app.get('/about', handlers.about);
 app.get('/section-test', handlers.sectionTest);
+app.get('/newsletter', handlers.newsletter);
 app.get('/newsletter-signup', handlers.newsletterSignup);
-app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
 app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+
+
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+        if (err) return res.status(500).send({error: err.message});
+        handlers.vacationPhotoContestProcess(req, res, fields, files);
+    });
+});
+
+
 app.use(express.static(__dirname + '/public'))
-//app.use(handlers.notFound);
-//app.use(handlers.serverError);
+app.use(handlers.notFound);
+app.use(handlers.serverError);
 
 if (require.main === module) {
     app.listen(port, () => {
